7f650237643b207835014b3df4c653116d19209a
Kavindu-Hasintha <mkavinduh@gmail.com>
2023-10-30 13:57:18 +0530

    User model added

---
+++ Authentication and Authorization with JWT/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication_and_Authorization_with_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
